64e64627a37252f177307c975fdc298bfd2dcc56
League Student <league@iMac-8.attlocal.net>
2018-11-02 16:55:53 -0700

    Vault (IP)

--- src/VJamesBond.java
+++ src/VJamesBond.java
@@ -1,4 +1,11 @@
 
 public class VJamesBond {
-
+int findCode(Vault bigVault) {
+for(int i = 0; i < 1000000; i++) {
+	if(bigVault.tryCode(i)) {
+		return i; 
+	}
+}
+return -1;
+}
 }
--- src/Vault.java
+++ src/Vault.java
@@ -1,4 +1,13 @@
 
 public class Vault {
-
+int randomNumber = new Random.nextInt(1000000);
+	int secretCode = randomNumber;
+boolean tryCode(int code) {
+if(code==secretCode) {
+	return true;
+}
+else {
+	return false;
+}
+}
 }
